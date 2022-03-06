990c15ce640272f13ffdeddb3cce6c1e19c4def4
caxton-m <caxymuchono@gmail.com>
2022-03-06 13:30:56 -0600

    Added the test function in index.js

--- index.js
+++ index.js
@@ -119,3 +119,9 @@ app.delete('/deleteTweets', function (req,res){
 app.listen(PORT, function() {
     console.log(`Example app listening on port ` + PORT);
 });
+
+function test_print(){
+
+    console.log('test code')
+
+}
